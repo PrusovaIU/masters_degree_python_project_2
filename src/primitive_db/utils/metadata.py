c637c136801e21f3b682a00c9f7d14777def66ff
Iuliia <prusova.work@gmail.com>
2025-10-22 21:09:43 +0300

    доработана функция load_metadata. добавлена функция save_metadata

--- src/primitive_db/utils/metadata.py
+++ src/primitive_db/utils/metadata.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 from src.primitive_db.schemas.db_meta import Database
-from json import load, JSONDecodeError
-from loguru import logger
+from json import load, JSONDecodeError, dump
 
 
 class GetMetadataError(Exception):
@@ -12,19 +11,17 @@ class MetadataFileError(GetMetadataError):
     pass
 
 
-class MetadataTypeError(GetMetadataError):
-    pass
-
-
-def _open_file(filepath: Path) -> dict:
+def load_metadata(filepath: Path) -> Database:
     """
-    Чтение метаданных из файла.
+    Загрузка метаданных о базе данных.
 
-    :param filepath: путь к файлу.
+    :param filepath: путь до файла с метаданными.
     :return: словарь с метаданными.
 
-    :raises GetMetadataError: если не удалось получить метаданные.
+    :raises GetMetadataError: если не удалось загрузить метаданные.
     """
+    if not filepath.exists():
+        return {}
     try:
         with filepath.open() as file:
             data = load(file)
@@ -36,14 +33,21 @@ def _open_file(filepath: Path) -> dict:
     return data
 
 
-def _parse_metadata(data: dict) -> Database:
-    if not isinstance(data, dict):
-        raise MetadataTypeError(
-            f"Metadata in file must be an object, "
-            f"but {data.__class__.__name__} was given"
+def save_metadata(filepath: Path, data: dict) -> None:
+    """
+    Сохранение метаданных о базе данных.
+
+    :param filepath: путь до файла с метаданными.
+    :param data: метаданные.
+    :return: None.
+    :raises MetadataFileError: если не удалось сохранить метаданные.
+    """
+    try:
+        with filepath.open("w") as file:
+            dump(data, file, indent=4, ensure_ascii=False)
+    except OSError as err:
+        raise MetadataFileError(
+            f"Cannot save metadata to {filepath}: "
+            f"{err} ({err.__class__.__name__})"
         )
-
-
-
-def load_metadata(filepath: Path) -> Database:
 
