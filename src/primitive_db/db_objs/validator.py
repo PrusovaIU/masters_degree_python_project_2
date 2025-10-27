91e7b6145720bef92259433146d9e3af8e646524
Iuliia <prusova.work@gmail.com>
2025-10-27 11:52:08 +0300

    доработка doc-string для валидации

--- src/primitive_db/db_objs/validator.py
+++ src/primitive_db/db_objs/validator.py
@@ -10,12 +10,36 @@ _ValidatorsType = dict[str, _ClassValidatorsType]
 
 
 class FieldValidator:
+    """
+    Класс для регистрации валидаторов полей.
+
+    :param field_name: имя поля для валидации.
+
+    Пример использования:
+
+        from db_objs.db_object import Model
+
+        class MyClass(Model):
+
+            @field_validator('field_name')
+            def validate_field(self, value: Any) -> Any:
+                if isinstance(value, int):
+                    return value + 1
+                return value
+    """
+    # список валидаторов:
     _validators: _ValidatorsType = {}
 
     def __init__(self, field_name: str):
         self._field_name = field_name
 
     def __call__(self, func: FieldValidatorType):
+        """
+        Регистрация валидатора.
+
+        :param func: функция-валидатор.
+        :return: func без изменений.
+        """
         class_name = func.__qualname__.split('.')[0]
         self._validators.setdefault(class_name, {})
         self._validators[class_name][self._field_name] = func
